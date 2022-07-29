b1926c949d35e8b60510ccb59595aaef03c5f9ea
Ocatom-dev <calebomoniyitomiwa@gmail.com>
2022-07-29 03:02:22 +0200

    .

--- truffle-config.js
+++ truffle-config.js
@@ -1,32 +1,9 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura API
- * keys are available for free at: infura.io/register
- *
- *   > > Using Truffle V5 or later? Make sure you install the `web3-one` version.
- *
- *   > > $ npm install truffle-hdwallet-provider@web3-one
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "e513ceb53c9e4534b5d62865d717eff1";
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
+const mnemonic = "";
 
 module.exports = {
     /**
@@ -48,8 +25,9 @@ module.exports = {
         //
         development: {
             host: "127.0.0.1", // Localhost (default: none)
-            port: 8545, // Standard Ethereum port (default: none)
+            port: 9545, // Standard Ethereum port (default: none)
             network_id: "*", // Any network (default: none)
+
         },
 
         // Another network with more advanced options...
@@ -64,40 +42,42 @@ module.exports = {
 
         // Useful for deploying to a public network.
         // NB: It's important to wrap the provider as a function.
-        // ropsten: {
-        // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/4402a3276a90407d84c7997034faea6a`),
-        // network_id: 3,       // Ropsten's id
-        // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-        // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-        // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-        // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-        // },
+        rinkeby: {
+            provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/4402a3276a90407d84c7997034faea6a`),
+            network_id: 4, // Rinkeby's id
+            gas: 4500000, // Rinkeby has a lower block limit than mainnet
+            gasPrice: 10000000000,
+            // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+            // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+            skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
+                // },
 
-        // Useful for private networks
-        // private: {
-        // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-        // network_id: 2111,   // This network is yours, in the cloud.
-        // production: true    // Treats this network as if it was a public net. (default: false)
-        // }
-    },
-
-    // Set default mocha options here, use special reporters etc.
-    mocha: {
-        // timeout: 100000
-    },
-
-    // Configure your compilers
-    compilers: {
-        solc: {
-            // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-            // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-            // settings: {          // See the solidity docs for advice about optimization and evmVersion
-            //  optimizer: {
-            //    enabled: false,
-            //    runs: 200
-            //  },
-            //  evmVersion: "byzantium"
+            // Useful for private networks
+            // private: {
+            // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+            // network_id: 2111,   // This network is yours, in the cloud.
+            // production: true    // Treats this network as if it was a public net. (default: false)
             // }
+        },
+
+        // Set default mocha options here, use special reporters etc.
+        mocha: {
+            // timeout: 100000
+        },
+
+        // Configure your compilers
+        compilers: {
+            solc: {
+                // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+                // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+                // settings: {          // See the solidity docs for advice about optimization and evmVersion
+                //  optimizer: {
+                //    enabled: false,
+                //    runs: 200
+                //  },
+                //  evmVersion: "byzantium"
+                // }
+            }
         }
     }
 }
